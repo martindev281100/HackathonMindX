edb440e300cf675d78daa3c69a447c43e071096d
martindevox2811 <manhdaokhacmanh@gmail.com>
2020-10-16 23:56:18 +0700

    update authorization

---
+++ js/model.js
@@ -0,0 +1,57 @@
+const model = {}
+
+model.currentUser = undefined;
+
+model.register = async (data) => {
+    try {
+        const response = await firebase.auth().createUserWithEmailAndPassword(data.email, data.password);
+        firebase.auth().currentUser.updateProfile({
+            displayName: data.userName,
+        });
+        firebase.auth().currentUser.sendEmailVerification();
+        firebase.auth().signOut()
+    } catch (err) {
+        alert(err.message);
+    }
+};
+
+model.login = async ({
+    email,
+    password
+}) => {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(error => {
+        alert(error.message)
+    });
+};
+
+model.logInWithGoogle = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    const response = firebase.auth().signInWithPopup(provider).then(function (result) {
+        firebase.firestore().collection("users").doc(result.user.uid).get().then(function (doc) {
+            if (doc.exists) {
+                return
+            } else {}
+        })
+    }).catch(function (error) {
+        alert(error.message)
+    })
+}
+model.logInWithFacebook = () => {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(async function (result) {
+        if (firebase.auth().currentUser.emailVerified == false) {
+            await firebase.auth().currentUser.sendEmailVerification();
+            model.setOffline(result.user.uid)
+            firebase.auth().signOut()
+        }
+        firebase.firestore().collection("users").doc(result.user.uid).get().then(function (doc) {
+            if (doc.exists) {
+                return
+            } else {
+            }
+        })
+    }).catch(function (error) {
+        alert(error.message)
+    });
+}
---
+++ js/view.js
@@ -0,0 +1,48 @@
+const view = {}
+
+view.setActiveScreen = async (screenName) => {
+    switch (screenName) {
+        case "registerPage":
+            document.getElementById('app').innerHTML = component.registerPage;
+            const registerForm = document.getElementById("register-form");
+            registerForm.addEventListener("submit", function (e) {
+                e.preventDefault();
+                const data = {
+                    userName: registerForm.userName.value,
+                    email: registerForm.email.value,
+                    password: registerForm.password.value,
+                    confirmPassword: registerForm.confirmPassword.value,
+                };
+                controller.register(data);
+            });
+            document.getElementById("redirect-to-login").addEventListener("click", function () {
+                view.setActiveScreen("loginPage");
+            });
+            break;
+
+        case "loginPage":
+            document.getElementById("app").innerHTML = component.loginPage;
+            const loginForm = document.getElementById("login-form");
+            loginForm.addEventListener("submit", function (e) {
+                e.preventDefault();
+                const data = {
+                    email: loginForm.email.value,
+                    password: loginForm.password.value,
+                };
+                controller.login(data);
+            });
+            document.getElementById('btn_google').addEventListener('click', function () {
+                model.logInWithGoogle();
+            })
+            document.getElementById('btn_facebook').addEventListener('click', function () {
+                model.logInWithFacebook();
+            })
+            document.getElementById("redirect-to-register").addEventListener("click", function () {
+                view.setActiveScreen("registerPage");
+            });
+            break;
+    }
+}
+view.setErrorMessage = (elementId, content) => {
+    document.getElementById(elementId).innerText = content;
+};
