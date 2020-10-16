7beb1cebeeb03af79ee88bc0b61706d9c66ab4ab
manhlt99 <luuthemanhgch17279@gmail.com>
2020-10-17 02:39:47 +0700

    update

--- js/index.js
+++ js/index.js
@@ -228,7 +228,7 @@ window.onload = async () => {
                 view.setActiveScreen("registerPage");
             }
         } else {
-            view.setActiveScreen("homePage")
+            view.setActiveScreen("loginPage")
         }
 
     })
--- js/view.js
+++ js/view.js
@@ -18,6 +18,9 @@ view.setActiveScreen = async (screenName) => {
             document.getElementById("redirect-to-login").addEventListener("click", function () {
                 view.setActiveScreen("loginPage");
             });
+            document.querySelector('.logIn').addEventListener('click', () => {
+                view.setActiveScreen("loginPage");
+            })
             break;
 
         case "loginPage":
@@ -40,6 +43,34 @@ view.setActiveScreen = async (screenName) => {
             document.getElementById("redirect-to-register").addEventListener("click", function () {
                 view.setActiveScreen("registerPage");
             });
+            document.querySelector('.logIn').addEventListener('click', () => {
+                view.setActiveScreen("registerPage");
+            })
+
+            // Get the modal
+            let modal = document.getElementById("myModal");
+
+            // Get the button that opens the modal
+            let btn = document.getElementById("myBtn");
+
+            // Get the <span> element that closes the modal
+            let span = document.getElementsByClassName("close")[0];
+            // When the user clicks on the button, open the modal
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+            
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+            
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                modal.style.display = "none";
+                }
+            }
             break;
 
         case "homePage":
@@ -79,4 +110,8 @@ view.showSlides = (n) => {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+
+
+
+
 
