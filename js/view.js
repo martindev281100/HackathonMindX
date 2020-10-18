27dcb49f9ff9f3913f4ba3a0c79c7584f4d27c41
mitximinaj <thangflymit@gmail.com>
2020-10-18 07:55:16 +0700

    .

--- js/view.js
+++ js/view.js
@@ -45,26 +45,15 @@ view.setActiveScreen = async (screenName) => {
             document.querySelector('.logIn').addEventListener('click', () => {
                 view.setActiveScreen("registerPage");
             })
-
-            // Get the modal
             let modal = document.getElementById("myModal");
-
-            // Get the button that opens the modal
             let btn = document.getElementById("myBtn");
-
-            // Get the <span> element that closes the modal
             let span = document.getElementsByClassName("close")[0];
-            // When the user clicks on the button, open the modal
             btn.onclick = function () {
                 modal.style.display = "block";
             }
-
-            // When the user clicks on <span> (x), close the modal
             span.onclick = function () {
                 modal.style.display = "none";
             }
-
-            // When the user clicks anywhere outside of the modal, close it
             window.onclick = function (event) {
                 if (event.target == modal) {
                     modal.style.display = "none";
@@ -74,7 +63,6 @@ view.setActiveScreen = async (screenName) => {
                 const resetPasswordEmail = document.getElementById('reset-password-email').value
                 model.sendPasswordResetEmail(resetPasswordEmail)
             })
-
             break;
 
         case "homePage":
